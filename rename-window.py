9420533c1fa478b4dd2517d5c953fd8f1e0e586f
mustapha-rashiduddin <chumchumchira@gmail.com>
2026-08-17 20:07:39 +0500

    rename-window.py: use workspace current for instant rename update (causes screen flicker)

--- rename-window.py
+++ rename-window.py
@@ -88,7 +88,7 @@ def main():
     overlay = load_overlay()
     overlay[con_id] = new_name
     save_overlay(overlay)
-    subprocess.run(["i3-msg", "nop"])
+    subprocess.run(["i3-msg", "workspace", "current"])
 
 
 if __name__ == "__main__":
